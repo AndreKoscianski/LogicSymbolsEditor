81911d7aaba6f704070da5ccdb491b0356e8efad
AndreKoscianski <koscianski@utfpr.edu.br>
2022-06-27 09:18:44 -0300

    Add files via upload

--- source/unit1.pas
+++ source/unit1.pas
@@ -15,13 +15,13 @@ type
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
-    Label1: TLabel;
     M: TMemo;
     OpenDialog1: TOpenDialog;
     SaveDialog1: TSaveDialog;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
     procedure MChange(Sender: TObject);
     procedure MKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
   private
@@ -44,10 +44,10 @@ procedure TForm1.MKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
 var i: integer;
     s: string;
 begin
-   case Key of
-   VK_MULTIPLY  : M.Lines.Text := StringReplace(M.Lines.Text,'*', '∧', [rfReplaceAll]);
-   VK_ADD       : M.Lines.Text := StringReplace(M.Lines.Text,'+', '∨', [rfReplaceAll]);
-   end
+   //case Key of
+   //VK_MULTIPLY  : M.Lines.Text := StringReplace(M.Lines.Text,'*', '∧', [rfReplaceAll]);
+   //VK_ADD       : M.Lines.Text := StringReplace(M.Lines.Text,'+', '∨', [rfReplaceAll]);
+   //end
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -57,8 +57,10 @@ begin
     M.Lines.Text := StringReplace(M.Lines.Text,'-', '¬', [rfReplaceAll]);
     M.Lines.Text := StringReplace(M.Lines.Text,'>', '→', [rfReplaceAll]);
     M.Lines.Text := StringReplace(M.Lines.Text,'<', '↔', [rfReplaceAll]);
-    M.Lines.Text := StringReplace(M.Lines.Text,'_', '⊦', [rfReplaceAll]);
+    M.Lines.Text := StringReplace(M.Lines.Text,'_', '⊥', [rfReplaceAll]);
+    M.Lines.Text := StringReplace(M.Lines.Text,'|', '⊦', [rfReplaceAll]);
     M.Lines.Text := StringReplace(M.Lines.Text,'=', '⊨', [rfReplaceAll]);
+
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
@@ -71,6 +73,11 @@ procedure TForm1.Button3Click(Sender: TObject);
 begin
   if SaveDialog1.Execute then
     M.Lines.SaveToFile(SaveDialog1.FileName);
+end;
+
+procedure TForm1.Label1Click(Sender: TObject);
+begin
+
 end;
 
 procedure TForm1.MChange(Sender: TObject);
